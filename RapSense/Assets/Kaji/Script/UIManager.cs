0309b73ad6f91f03d894f6a83f639b29d453d5ad
Kaji Satoshi <s1210006@gmail.com>
2016-01-16 15:50:08 +0900

    UIのモック的なの

---
+++ RapSense/Assets/Kaji/Script/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject Panel1, Panel2;
+	[SerializeField]
+	private int nowPlayer = 1;
+
+	void Start(){
+	}
+
+	void ChangeTurn(){
+		if(nowPlayer == 1){
+			Panel1.SetActive(false);
+			Panel2.SetActive(true);
+			nowPlayer = 2;
+		}if(nowPlayer == 2){
+			Panel2.SetActive(false);
+			Panel1.SetActive(true);
+			nowPlayer = 1;
+		}
+	}
+}
