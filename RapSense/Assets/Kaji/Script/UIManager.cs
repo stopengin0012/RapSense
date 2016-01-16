3f4ef72365ac9209154a1cb1807bea4d806e8d3c
Kaji Satoshi <s1210006@gmail.com>
2016-01-16 21:06:25 +0900

    カウントダウンの追加

--- RapSense/Assets/Kaji/Script/UIManager.cs
+++ RapSense/Assets/Kaji/Script/UIManager.cs
@@ -8,11 +8,14 @@ public class UIManager : MonoBehaviour {
 	private GameObject Panel1, Panel2;
 	[SerializeField]
 	private int nowPlayer = 0;
+	[SerializeField]
+	private Text textCountdown;
 
 	void Start(){
 		Panel1.SetActive(false);
 		Panel2.SetActive(true);
 		nowPlayer = 1;
+		StartCoroutine(CountdownCoroutine());
 	}
 
 	void ChangeTurn(){
@@ -30,5 +33,29 @@ public class UIManager : MonoBehaviour {
 	IEnumerator ChangeTurnSpan(float sec){
 		yield return new WaitForSeconds(sec);
 		ChangeTurn();
+	}
+	public void OnClickButtonStart()
+	{
+		StartCoroutine(CountdownCoroutine());
+	}
+
+IEnumerator CountdownCoroutine()
+	{
+		textCountdown.gameObject.SetActive(true);
+
+		textCountdown.text = "3";
+		yield return new WaitForSeconds(1.0f);
+
+		textCountdown.text = "2";
+		yield return new WaitForSeconds(1.0f);
+
+		textCountdown.text = "1";
+		yield return new WaitForSeconds(1.0f);
+
+		textCountdown.text = "GO!";
+		yield return new WaitForSeconds(1.0f);
+
+		textCountdown.text = "";
+		textCountdown.gameObject.SetActive(false);
 	}
 }
