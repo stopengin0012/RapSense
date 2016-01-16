f1077401a85a355543f976b466c82d0db591d0a5
Kaji Satoshi <s1210006@gmail.com>
2016-01-16 18:24:31 +0900

    save

--- RapSense/Assets/Kaji/Script/UIManager.cs
+++ RapSense/Assets/Kaji/Script/UIManager.cs
@@ -7,20 +7,28 @@ public class UIManager : MonoBehaviour {
 	[SerializeField]
 	private GameObject Panel1, Panel2;
 	[SerializeField]
-	private int nowPlayer = 1;
+	private int nowPlayer = 0;
 
 	void Start(){
+		Panel1.SetActive(false);
+		Panel2.SetActive(true);
+		nowPlayer = 1;
 	}
 
 	void ChangeTurn(){
 		if(nowPlayer == 1){
+			Panel1.SetActive(true);
+			Panel2.SetActive(false);
+			nowPlayer = 2;
+		}else if(nowPlayer == 2){
 			Panel1.SetActive(false);
 			Panel2.SetActive(true);
-			nowPlayer = 2;
-		}if(nowPlayer == 2){
-			Panel2.SetActive(false);
-			Panel1.SetActive(true);
 			nowPlayer = 1;
 		}
+	}
+
+	IEnumerator ChangeTurnSpan(float sec){
+		yield return new WaitForSeconds(sec);
+		ChangeTurn();
 	}
 }
