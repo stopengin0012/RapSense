28f05b78b6d28403b944278807f83444e4946f86
Kaji Satoshi <s1210006@gmail.com>
2016-01-20 03:51:18 +0900

    iTweenの追加

--- RapSense/Assets/Kaji/Script/UIManager.cs
+++ RapSense/Assets/Kaji/Script/UIManager.cs
@@ -12,11 +12,19 @@ public class UIManager : MonoBehaviour {
 	private Text textCountdown, textCountdown2;
 	[SerializeField]
 	private GameObject nowText1,nowText2;
+	[SerializeField]
+	private Image[] nowStage;
+	[SerializeField]
+	private int nowStageNum=0;
+	[SerializeField]
+	private bool endStage = false;
 
 	void Start(){
-		Panel1.SetActive(false);
-		Panel2.SetActive(true);
 		ChangeTurn();
+	}
+
+	void Update(){
+
 	}
 
 	void ChangeTurn(){
@@ -26,6 +34,7 @@ public class UIManager : MonoBehaviour {
 			nowPlayer = 2;
 			StartCoroutine(CountdownCoroutine(textCountdown2));
 		}else{
+			endStage = true;
 			Panel1.SetActive(false);
 			Panel2.SetActive(true);
 			nowPlayer = 1;
@@ -41,30 +50,22 @@ public class UIManager : MonoBehaviour {
 
 	IEnumerator CountdownCoroutine(Text countdown)
 	{
-
 		yield return new WaitForSeconds(2f);
-
 		countdown.gameObject.SetActive(true);
-
 		countdown.text = "3";
 		yield return new WaitForSeconds(1.0f);
-
 		countdown.text = "2";
 		yield return new WaitForSeconds(1.0f);
-
 		countdown.text = "1";
 		yield return new WaitForSeconds(1.0f);
-
 		countdown.text = "GO!";
 		yield return new WaitForSeconds(1.0f);
-
 		countdown.text = "";
 		countdown.gameObject.SetActive(false);
 		if(nowPlayer == 1){
 			StartCoroutine(ChangeTurnSpan(nowText1.GetComponent<TextSpan>().spanTime));
 		}else{
 			StartCoroutine(ChangeTurnSpan(nowText2.GetComponent<TextSpan>().spanTime));
-
 		}
 	}
 }
